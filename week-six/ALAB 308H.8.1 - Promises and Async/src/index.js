edc2f16d58c47f9b7e6eb858df439e56a00a876a
aarti24sh <119397389+aarti24sh@users.noreply.github.com>
2023-06-14 22:44:31 -0700

    promises and Async assignment

---
+++ week-six/ALAB 308H.8.1 - Promises and Async/src/index.js
@@ -0,0 +1,28 @@
+// Importing database functions. DO NOT MODIFY THIS LINE.
+import { central, db1, db2, db3, vault } from "./databases.js";
+
+async function getUserData(id) {
+  const dbs = {
+    db1: db1,
+    db2: db2,
+    db3: db3
+  };
+
+  const dbId = await central(id);
+
+  //const userInformation = await dbs[dbId](id);
+  //const vaultInformation = await vault(id);
+
+  Promise.all([await dbs[dbId](id), await vault(id)]).then(([userInformation, vaultInformation]) => {
+    let userData = {};
+    userData.userInformation = userInformation;
+    userData.vaultInformation = vaultInformation;
+    console.log(userData);
+  });
+  
+  //console.log(userInformation);
+  //console.log(vaultInformation);
+}
+
+let user = getUserData(3);
+
